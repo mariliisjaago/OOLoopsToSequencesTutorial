fffec62daa4bd5c88e6c4065ba5308a4134d8ef6
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-04-24 12:05:57 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using PainterLibrary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static IPainter FindCheapest(double sqMeters, IEnumerable<IPainter> painters)
+        {
+
+        }
+    }
+}
---
+++ PainterLibrary/HousePainter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PainterLibrary
+{
+    public class HousePainter : IPainter
+    {
+        public bool IsAvailable { get; set; }
+
+        public double EstimateCost(double sqMeters)
+        {
+            return 5 * sqMeters;
+        }
+
+        public TimeSpan EstimateTimeToPaint(double sqMeters)
+        {
+            TimeSpan oneSqMeterTimeSpan = TimeSpan.FromHours(2);
+
+            return 5 * oneSqMeterTimeSpan;
+        }
+    }
+}
