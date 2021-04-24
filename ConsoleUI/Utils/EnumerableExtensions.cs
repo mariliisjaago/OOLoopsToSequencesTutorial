fe9ae83db79ae9e33dc9ac3add91c40bea8a6182
Mariliis Jaago <mariliisjaago@hotmail.com>
2021-04-24 12:43:22 +0300

    Practiced writing a similar extension method for return an element with the max value.

--- ConsoleUI/Utils/EnumerableExtensions.cs
+++ ConsoleUI/Utils/EnumerableExtensions.cs
@@ -21,5 +21,20 @@ namespace ConsoleUI.Utils
 
             return output;
         }
+
+        public static T WithMaximum<T, TKey>(this IEnumerable<T> sequence, Func<T, TKey> criterion)
+            where T : class
+            where TKey : IComparable<TKey>
+        {
+            var output = sequence.Select(x => Tuple.Create(x, criterion(x)))
+                .Aggregate(
+                    (Tuple<T, TKey>)null,
+                    (best, cur) =>
+                    best == null || cur.Item2.CompareTo(best.Item2) > 0 ? cur : best
+                    )
+                .Item1;
+
+            return output;
+        }
     }
 }
